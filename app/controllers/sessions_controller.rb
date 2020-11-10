adf5a209aa50b45e24fdd42a262658372fd07a8f
naoki-tsunekawa <b1014138@gmail.com>
2020-11-10 23:11:28 +0900

    create Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,19 +5,25 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
-      # remember meのチェックボックスの値によってcookieに保存するかどうか判別
-      # if params[:session][:remember_me] == '1'
-      #   remember(user)
-      # else
-      #   forget(user)
-      # end
-      # ↑同じ意味
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      # フレンドリーフォワーディング
-      # ログインする前に記憶したurlもしくはデフォルトのurlへリダイレクトする
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        # # remember meのチェックボックスの値によってcookieに保存するかどうか判別
+        # # if params[:session][:remember_me] == '1'
+        # #   remember(user)
+        # # else
+        # #   forget(user)
+        # # end
+        # # ↑同じ意味
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        # フレンドリーフォワーディング
+        # ログインする前に記憶したurlもしくはデフォルトのurlへリダイレクトする
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -18,14 +18,14 @@ RSpec.describe "Users", type: :request do
 
   # ユーザ新規作成テスト
   describe 'create' do
-    context 'valid request' do
+    describe 'valid request' do
       it 'adds a user' do
         expect do
           post signup_path, params: { user: FactoryBot.attributes_for(:user) }
         end.to change(User, :count).by(1)
       end
 
-      context 'adds a user' do
+      describe 'sign in response check' do
         before { post signup_path, params: { user: FactoryBot.attributes_for(:user) } }
         subject { response }
 
@@ -36,6 +36,50 @@ RSpec.describe "Users", type: :request do
           expect(is_logged_in?).to be_truthy
         end
 
+      end
+    end
+
+    describe "#create" do
+      include ActiveJob::TestHelper
+
+      it "is invalid with invalid signup information" do
+        perform_enqueued_jobs do
+          expect {
+            post users_path, params: { user: { name: "",
+                                              email: "user@invalid",
+                                              password: "foo",
+                                              password_confirmation: "bar" } }
+          }.to_not change(User, :count)
+        end
+      end
+
+      it "is valid with valid signup information" do
+        perform_enqueued_jobs do
+          expect {
+            post users_path, params: { user: { name: "ExampleUser",
+                                              email: "user@example.com",
+                                              password: "password",
+                                              password_confirmation: "password" } }
+          }.to change(User, :count).by(1)
+
+          expect(response).to redirect_to root_path
+          user = assigns(:user)    # gem 'rails-controller-testing'をインストール
+          # 有効化していない状態でログインしてみる
+          sign_in_as(user)
+          expect(session[:user_id]).to be_falsey
+          # 有効化トークンが不正な場合
+          get edit_account_activation_path("invalid token", email: user.email)
+          expect(session[:user_id]).to be_falsey
+          # トークンは正しいがメールアドレスが無効な場合
+          get edit_account_activation_path(user.activation_token, email: 'wrong')
+          expect(session[:user_id]).to be_falsey
+          # 有効化トークンが正しい場合
+          get edit_account_activation_path(user.activation_token, email: user.email)
+          expect(session[:user_id]).to eq user.id
+          expect(user.name).to eq "ExampleUser"
+          expect(user.email).to eq "user@example.com"
+          expect(user.password).to eq "password"
+        end
       end
     end
   end
